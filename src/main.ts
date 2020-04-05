4f237f3ccc1a8e9d73e91327d61b1b6e785d55bd
mablin7 <magyarb0201@gmail.com>
2020-04-05 20:37:29 +0200

    s

--- src/main.ts
+++ src/main.ts
@@ -40,10 +40,10 @@ function App() {
 
     if (title === '') {
         let pathArray = window.location.search.replace('?', '')
-        fetch(`/api/checkout_note?title=${pathArray[pathArray.length-1]}`)
+        fetch(`/api/checkout_note?title=${pathArray}`)
         .then(res => res.json())
-        .then(({n_title, content}) => {
-            setTitle(n_title)
+        .then(({title: n, content}) => {
+            setTitle(n)
             setCont(content)
         })
     }
